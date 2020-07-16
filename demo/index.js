b55bcb261aedb8e8ed14f0e86363edb040265ec2
Cheng Kuan <cheng.kuan2000@gmail.com>
2020-07-16 11:06:39 +0200

    webpack

---
+++ demo/index.js
@@ -0,0 +1,46 @@
+/**
+ * @flow
+ */
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import AnimatedText from '../dist'
+
+const App = () => {
+  const [disabled, setDisabled] = React.useState(true)
+
+  return (
+    <div>
+      <AnimatedText
+        text="Random Text Change"
+        tag="div"
+        type="random"
+        style={{ margin: 20 }}
+      />
+      <AnimatedText
+        text="Typing Text Change"
+        tag="div"
+        style={{ margin: 20 }}
+      />
+      <button style={{ margin: 20 }} onClick={() => setDisabled(! disabled)}>Click here to toggle below</button>
+      <AnimatedText
+        disabled={disabled}
+        text="Disabled Random Text Change"
+        tag="div"
+        type="random"
+        style={{ margin: 20 }}
+      />
+      <AnimatedText
+        disabled={disabled}
+        text="Disabled Typing Text Change"
+        tag="div"
+        style={{ margin: 20 }}
+      />
+    </div>
+  )
+}
+
+const wrapper = document.getElementById("main")
+wrapper && ReactDOM.render(<App />, wrapper)
