ae9d9643040e446638dbb587149618da114267f3
Cheng Kuan <ngai.cheng.kuan@etron.at>
2025-08-11 15:25:17 +0200

    got rid of memo

--- src/ChangingText.jsx
+++ src/ChangingText.jsx
@@ -1,8 +1,7 @@
-import { memo } from 'react'
 import styled, { css, keyframes } from 'styled-components'
 import { toUnicode } from './helper'
 
-const ChangingText = memo(styled(({ delay, disabled, random, speed, tag: Tag, text, textStyle, ...props }) => {
+const ChangingText = styled(({ delay, disabled, random, speed, tag: Tag, text, textStyle, ...props }) => {
   return <Tag style={{...textStyle, position: 'absolute', top: 0, left: 0 }} {...props} />
 })`&:before{
   ${({ disabled, speed, random, text }) => {
@@ -21,6 +20,6 @@ const ChangingText = memo(styled(({ delay, disabled, random, speed, tag: Tag, te
   display: block;
   -webkit-animation-delay: ${({delay}) => delay}ms;
   animation-delay: ${({delay}) => delay}ms;
-}`, (prevProps, nextProps) => prevProps.disabled == nextProps.disabled && prevProps.text == nextProps.text)
+}`
 
 export default ChangingText
